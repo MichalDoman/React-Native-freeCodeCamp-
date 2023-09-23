a0aa87de1ada015dbab8d2fb9fa822de9c7a6dc3
michaldoman <mjd.domanski@gmail.com>
2023-09-23 16:24:26 +0200

    more styles and components

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -6,9 +6,16 @@ const App = () => {
         <SafeAreaView style={styles.wrapper}>
             <View style={styles.container}>
                 <Text>Current Weather</Text>
-                <Text>6</Text>
-                <Text>Feels like 5</Text>
-                <Text>High: 8 Low: 6</Text>
+                <Text style={styles.temp}>6</Text>
+                <Text style={styles.feels}>Feels like 5</Text>
+                <View style={styles.highLowWrapper}>
+                    <Text style={styles.highLow}>High: 8</Text>
+                    <Text style={styles.highLow}>Low: 6</Text>
+                </View>
+            </View>
+            <View style={styles.bodyWrapper}>
+                <Text style={styles.description}>It's sunny</Text>
+                <Text style={styles.message}>Perfect weather</Text>
             </View>
         </SafeAreaView>
     )
@@ -19,9 +26,35 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     container: {
-        backgroundColor: 'pink',
+        backgroundColor: 'red',
         flex: 1,
         alignItems: 'center',
-    }
+        paddingTop: 40,
+    },
+    temp: {
+        color: 'black',
+        fontSize: 48,
+    },
+    feels: {
+        color: 'black',
+        fontSize: 30,
+    },
+    highLowWrapper: {
+      flexDirection: 'row',
+    },
+    highLow: {
+        color: 'black',
+        fontSize: 20,
+    },
+    bodyWrapper: {
+        justifyContent: 'flex-end',
+        alignItems: 'flex-start',
+    },
+    description: {
+
+    },
+    message: {
+
+    },
 })
 export default App
