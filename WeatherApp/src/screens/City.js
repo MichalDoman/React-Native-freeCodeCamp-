fc9319ce48f3222d67cf697dc1fb6d89fa526041
michaldoman <mjd.domanski@gmail.com>
2023-09-27 18:49:21 +0200

    City styles + style merging

--- WeatherApp/src/screens/City.js
+++ WeatherApp/src/screens/City.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {SafeAreaView, Text, ImageBackground, StyleSheet, StatusBar} from "react-native";
+import {SafeAreaView, Text, ImageBackground, StyleSheet, StatusBar, View} from "react-native";
+import {Feather} from '@expo/vector-icons'
 
 const City = () => {
     return (
@@ -8,7 +9,18 @@ const City = () => {
                 source={require('../../assets/city.jpg')}
                 style={styles.imageLayout}
             >
-                <Text>City</Text>
+                <Text style={[styles.cityText, styles.cityName]}>Warsaw</Text>
+                <Text style={[styles.cityText, styles.countryName]}>PL</Text>
+                <View style={styles.populationWrapper}>
+                    <Feather name={'user'} size={50} color={'red'}/>
+                    <Text style={styles.populationText}>2,000,000</Text>
+                </View>
+                <View style={styles.riseSetWrapper}>
+                    <Feather name={'sunrise'} size={50} color={'white'}/>
+                    <Text style={styles.riseSetText}>10:46:58am</Text>
+                    <Feather name={'sunset'} size={50} color={'white'}/>
+                    <Text style={styles.riseSetText}>17:28:50pm</Text>
+                </View>
             </ImageBackground>
         </SafeAreaView>
     )
@@ -19,8 +31,43 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: StatusBar.currentHeight || 0,
     },
+    cityText: {
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    cityName: {
+        fontSize: 40,
+    },
+    countryName: {
+        fontSize: 30,
+    },
     imageLayout: {
         flex: 1,
+    },
+    populationWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30
+    },
+    populationText: {
+        fontSize: 25,
+        marginLeft: 7.5,
+        color: 'red',
+        fontWeight: 'bold'
+    },
+    riseSetWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginTop: 30,
+    },
+    riseSetText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
     }
 })
 export default City
