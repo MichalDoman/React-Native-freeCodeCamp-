affdba4b4994a146f1c1e7f7192dd318bc202ba4
michaldoman <mjd.domanski@gmail.com>
2023-10-07 18:13:43 +0200

    fetchWeatherData function

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -9,6 +9,16 @@ const App = () => {
     const [loading, setLoading] = useState(true)
     const [location, setLocation] = useState(null)
     const [error, setError] = useState(null)
+    const [weather, setWeather] = useState([])
+
+    const fetchWeatherData = async() => {
+        const response = await fetch(
+            `http://api.openweathermap.org/data/2.5/forecast?lat={${location.coords.latitude}}&lon={${location.coords.longitude}}&appid={${WEATHER_API_KEY}}`
+        )
+        const data = await response.json()
+        setWeather(data)
+        setLoading(false)
+    }
 
     useEffect(() => {
         (async() => {
@@ -19,6 +29,7 @@ const App = () => {
             }
             let location = await Location.getCurrentPositionAsync({})
             setLocation(location)
+            await fetchWeatherData()
         })()
     })
 
