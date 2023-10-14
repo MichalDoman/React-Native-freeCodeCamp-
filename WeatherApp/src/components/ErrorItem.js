f7d0d44a0e54ac47d8f3cf3f10f64502507a12f8
michaldoman <mjd.domanski@gmail.com>
2023-10-14 14:02:48 +0200

    Error handling - set up

---
+++ WeatherApp/src/components/ErrorItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from "react-native";
+
+const ErrorItem = () => {
+    return (
+
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+export default ErrorItem
